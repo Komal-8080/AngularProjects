c57a9077279b164c4c14893ac9cd257b2d62af09
Komal-8080 <shindeg163@gmail.com>
2021-03-16 15:23:05 +0530

    ADDED: Using Input Event Binding validating user name

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { threadId } from 'node:worker_threads';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ export class AppComponent {
   imgUrl;
   url;
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello From Bridgelabz.";
@@ -20,5 +22,15 @@ export class AppComponent {
   onClick($event) {
     console.log("Save button is Clicked!!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event) {
+    console.log("Change Event Occurred", $event.data);
+    const nameRegex = RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
+    if(nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
   }
 }
