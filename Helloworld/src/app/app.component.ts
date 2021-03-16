06a8439f06e38de8994f7fef347dccc2c75e1092
Komal-8080 <shindeg163@gmail.com>
2021-03-16 14:39:21 +0530

    ADDED: Using Event Binding to launch other site on clicking on logo

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Helloworld';
   imgUrl="";
+  url=""
 
   ngOnInit(): void {
     this.title = "Hello From Bridgelabz.";
     this.imgUrl = "../assets/BridgelabzLogo.png";
+    this.url = "https://www.bridgelabz.com";
+  }
+
+  onClick($event) {
+    console.log("Save button is Clicked!!", $event);
+    window.open(this.url, "_blank");
   }
 }
